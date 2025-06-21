8bb201d1d076c66191255d84a9c58fa7637b3711
Dushyant Makwana <dushyantmakwana2004@gmail.com>
2025-06-21 12:03:13 +0530

    Second Commit

---
+++ models/Urls.js
@@ -0,0 +1,20 @@
+const mongoose= require("mongoose");
+const urlSchema = new mongoose.Schema({
+    originalUrl: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    shortUrl: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    clicks: {
+        type: Number,
+        default: 0
+    }    
+});
+
+module.exports= mongoose.model("Url", urlSchema);
+// This code defines a Mongoose schema for a URL shortener application.
--- server.js
+++ server.js
@@ -1,9 +1,12 @@
-import express from 'express';
-import mongoose from 'mongoose';
+const express = require('express');
+const mongoose = require('mongoose');
+// Import the ShortUrl model
+const ShortUrl=require("./models/Urls") // Adjust the path as necessary
 
 mongoose.connect('mongodb://localhost:27017/URLShortener')
 
 const app = express();
+app.set("view engine", "ejs");
 
 app.get("/",(req,res)=>{
   res.render("FrontPage", { title: "Home Page" });
